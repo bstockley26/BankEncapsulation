db7319e818a687822b50a9a2b19a008180c8d8cd
Brooks Stockley <bstockley26@gmail.com>
2024-05-09 15:06:03 -0400

    finished

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+		private double _balance;
+
+		public void Deposit()
+		{
+            Console.WriteLine("What is the amount you are depositing?");
+
+            string inputAmount=Console.ReadLine();
+			int number;
+
+            bool success = int.TryParse(inputAmount, out number);
+
+			if (success)
+			{
+				Console.WriteLine($"{inputAmount} was added to your account!!");
+                _balance += number;
+            }
+			else
+			{
+				Console.WriteLine("Provide a accepted amount!");
+			}
+
+            
+		}
+		public void WithDraw()
+		{
+            Console.WriteLine("What is the amount you are withdrawing?");
+
+            string inputAmount = Console.ReadLine();
+			int number;
+
+            bool success = int.TryParse(inputAmount, out number);
+
+
+            if (success)
+			{
+				if (number < _balance)
+				{
+					_balance -= number;
+				}
+				else
+				{
+					Console.WriteLine("Insufficient funds! Choose available amount");
+					
+				}
+			}
+                
+            else
+            {
+                Console.WriteLine("Provide accepted amount!!");
+            }
+
+
+            
+		}
+		public double GetBalance()
+		{
+			return _balance;
+			
+		}
+
+
+
+
+	}
+
+
+
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var brooks = new BankAccount();
+            var money = brooks.GetBalance();
+
+            Console.WriteLine($"Account balance = {money}");
+
+            brooks.Deposit();
+
+            money = brooks.GetBalance();
+            Console.WriteLine($"Account balance = {money}");
+
+
+            brooks.WithDraw();
+
+            money = brooks.GetBalance();
+            Console.WriteLine($"Account balance = {money}");
+
+
+            //Console.WriteLine($"Hello. Pick the appropriate number of the following for how best we can help you." +
+            //    $" 1.Balance, 2.Withdraw, 3. Deposit");
+            //var actionNumber = Console.ReadLine();
+            //int number;
+            //if (int.TryParse(actionNumber, out number))
+            //{
+            //    switch (number)
+            //    {
+            //        case 1:
+            //            money = brooks.GetBalance();
+            //            Console.WriteLine($"Account balance = {money}");
+            //            break;
+
+            //        case 2:
+            //            brooks.WithDraw();
+            //            break;
+
+            //        case 3:
+            //            brooks.Deposit();
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Sorry can not help with that");
+            //            break;
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please choose appropriate number"); 
+            //}
+
+
+
         }
     }
 }
